eb817ab19f7a5e1c3d2581685e94dfe7a778e4a8
Francky Iside <francky@tuta.io>
2023-03-05 13:00:03 +0100

    put search input into a separate component

---
+++ src/components/SearchLocation.vue
@@ -0,0 +1,35 @@
+<template>
+  <q-page class="flex flex-center column">
+    <h3 text-h3>New York</h3>
+    <q-input
+      rounded
+      standout
+      bottom-slots
+      v-model="text"
+      label="Location"
+      counter
+    >
+      <template v-slot:prepend>
+        <q-icon name="place" />
+      </template>
+      <template v-slot:append>
+        <q-icon name="close" @click="text = ''" class="cursor-pointer" />
+      </template>
+
+      <template v-slot:hint> E.g. Rome </template>
+    </q-input>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "SearchLocation",
+  setup() {
+    return {
+      text: ref(""),
+    };
+  },
+});
+</script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,29 +1,18 @@
 <template>
-  <q-page class="flex flex-center">
-    <q-input
-      rounded
-      standout
-      bottom-slots
-      v-model="text"
-      label="Location"
-      counter
-    >
-      <template v-slot:prepend>
-        <q-icon name="place" />
-      </template>
-      <template v-slot:append>
-        <q-icon name="close" @click="text = ''" class="cursor-pointer" />
-      </template>
-
-      <template v-slot:hint> E.g. Rome </template>
-    </q-input>
-  </q-page>
+  <SearchLocation></SearchLocation>
 </template>
 
 <script>
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
+import SearchLocation from "src/components/SearchLocation.vue";
 
 export default defineComponent({
   name: "IndexPage",
+  setup() {
+    return {
+      text: ref(""),
+    };
+  },
+  components: { SearchLocation },
 });
 </script>
