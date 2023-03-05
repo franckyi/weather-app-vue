9183239d984d7592caeea8d6da8903a8cd16f998
Francky Iside <francky@tuta.io>
2023-03-05 21:50:13 +0100

    remove unused js

--- src/components/CurrentInfo.vue
+++ src/components/CurrentInfo.vue
@@ -61,56 +61,10 @@
 <script>
 import { ref, defineComponent } from "vue";
 
-const columns = [
-  {
-    name: "desc",
-    required: true,
-    label: "Dessert (100g serving)",
-    align: "left",
-    field: (row) => row.name,
-    format: (val) => `${val}`,
-    sortable: true,
-  },
-  {
-    name: "calories",
-    align: "center",
-    label: "Calories",
-    field: "calories",
-    sortable: true,
-  },
-  { name: "fat", label: "Fat (g)", field: "fat", sortable: true },
-  { name: "carbs", label: "Carbs (g)", field: "carbs" },
-];
-
-const rows = [
-  {
-    name: "Frozen Yogurt",
-    calories: 159,
-    fat: 6.0,
-    carbs: 24,
-  },
-  {
-    name: "Ice cream sandwich",
-    calories: 237,
-    fat: 9.0,
-    carbs: 37,
-  },
-  {
-    name: "Eclair",
-    calories: 262,
-    fat: 16.0,
-    carbs: 23,
-  },
-];
-
 export default defineComponent({
   name: "CurrentInfo",
   setup() {
-    return {
-      filter: ref(""),
-      columns,
-      rows,
-    };
+    return {};
   },
 });
 </script>
