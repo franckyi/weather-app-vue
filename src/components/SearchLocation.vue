b30939c07b99c577df87e074f6261502a6b06a81
Francky Iside <francky@tuta.io>
2023-03-07 00:58:20 +0100

    make search work - in progress

--- src/components/SearchLocation.vue
+++ src/components/SearchLocation.vue
@@ -17,15 +17,37 @@
   </q-input>
 </template>
 
-<script>
-import { defineComponent, ref } from "vue";
+<script setup>
+import { ref } from "vue";
+// import CurrentWeather from "./CurrentWeather.vue";
+import { useCurrentWeatherStore } from "../stores/current-weather.js";
+import { useCoreStore } from "../stores/core.js";
 
-export default defineComponent({
-  name: "SearchLocation",
-  setup() {
-    return {
-      text: ref(""),
-    };
-  },
-});
+const currentWeather = useCurrentWeatherStore();
+const core = useCoreStore();
+
+let text = ref(null);
+
+const getDifferentLatLon = () => {
+  fetch(
+    `http://api.openweathermap.org/geo/1.0/direct?q=${text.value}&limit=5&appid=${CurrentWeather.API_KEY}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      console.log(data.lat);
+      console.log(data.lon);
+      currentWeather.lat = data.lat;
+      currentWeather.lon = data.lon;
+    });
+};
+
+// export default defineComponent({
+//   name: "SearchLocation",
+//   setup() {
+//     return {
+//       text: ref(""),
+//     };
+//   },
+// });
 </script>
