6f91550c8bbb7b7206da20ca35da769fd5812226
Francky Iside <francky@tuta.io>
2023-03-07 23:14:26 +0100

    replace input template

--- src/components/SearchLocation.vue
+++ src/components/SearchLocation.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <q-input
     rounded
     dark
@@ -16,6 +16,31 @@
       <q-icon name="close" @click="text = ''" class="cursor-pointer" />
     </template>
   </q-input>
+  <q-select v-model="model" :options="options" label="Standard" />
+</template> -->
+
+<template>
+  <div class="q-pa-md">
+    <div class="q-gutter-md row">
+      <q-select
+        filled
+        v-model="model"
+        use-input
+        dark
+        input-debounce="0"
+        label="Simple filter"
+        :options="options"
+        @filter="filterFn"
+        style="width: 250px"
+      >
+        <template v-slot:no-option>
+          <q-item>
+            <q-item-section class="text-grey"> No results </q-item-section>
+          </q-item>
+        </template>
+      </q-select>
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -27,19 +52,45 @@ const currentWeather = useCurrentWeatherStore();
 const core = useCoreStore();
 
 let text = ref("");
+const stringOptions = [];
+const options = ref(stringOptions);
 let model = ref(null);
-let options = ["Google", "Facebook", "Twitter", "Apple", "Oracle"];
-let dense = ref(false);
-let denseOpts = ref(false);
+
+function filterFn(val, update) {
+  console.log(val);
+  if (val === "") {
+    update(() => {
+      options.value = stringOptions;
+
+      // here you have access to "ref" which
+      // is the Vue reference of the QSelect
+    });
+    return;
+  }
+
+  getResults();
+
+  update(() => {
+    const needle = val.toLowerCase();
+    options.value = stringOptions.filter(
+      (v) => v.toLowerCase().indexOf(needle) > -1
+    );
+  });
+}
 
 const getResults = () => {
+  console.warn("WORKS!");
   fetch(
-    `http://api.openweathermap.org/geo/1.0/direct?q=${text.value}&limit=5&appid=${core.API_KEY}`
+    `http://api.openweathermap.org/geo/1.0/direct?q=${val}&limit=5&appid=${core.API_KEY}`
   )
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
+      data.forEach(
+        (obj) => options.value.push(obj.name)
+        // ${obj.name}, ${obj.state}, ${obj.country}
+      );
     });
-  text.value = "";
+  // text.value = "";
 };
 </script>
