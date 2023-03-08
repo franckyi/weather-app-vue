80a93cdae31f62578e811c81a4d27b65abe93c5e
Francky Iside <francky@tuta.io>
2023-03-08 22:22:41 +0100

    create options - in progress

--- src/components/SearchLocation.vue
+++ src/components/SearchLocation.vue
@@ -16,6 +16,9 @@
       <q-icon name="close" @click="text = ''" class="cursor-pointer" />
     </template>
   </q-input>
+  <!-- <li v-for="r in results">
+    {{ (r.name, r.state, r.country) }}
+  </li> -->
 </template>
 
 <script setup>
@@ -27,18 +30,20 @@ const currentWeather = useCurrentWeatherStore();
 const core = useCoreStore();
 
 let text = ref("");
+const results = ref([]);
 
 const getResults = () => {
+  results.value = [];
   fetch(
     `http://api.openweathermap.org/geo/1.0/direct?q=${text.value}&limit=5&appid=${core.API_KEY}`
   )
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
-      // data.forEach(
-      // (obj) => options.value.push(obj.name)
-      // ${obj.name}, ${obj.state}, ${obj.country}
-      // );
+      data.forEach((obj) =>
+        results.value.push(`${obj.name}, ${obj.state}, ${obj.country}`)
+      );
+      console.log(results.value);
     });
   text.value = "";
 };
