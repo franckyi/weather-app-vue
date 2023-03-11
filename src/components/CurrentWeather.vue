6252c23e0eceeae3bc97a56e0c94ed09ecbf6c20
Francky Iside <francky@tuta.io>
2023-03-12 00:49:31 +0100

    style

--- src/components/CurrentWeather.vue
+++ src/components/CurrentWeather.vue
@@ -1,7 +1,7 @@
 <template>
   <h5>{{ currentWeather.name }}</h5>
   <h1>{{ currentWeather.temp }} °C</h1>
-  <p class="text-italic">feels like {{ currentWeather.tempFeel }}</p>
+  <p class="text-italic feel">feels like {{ currentWeather.tempFeel }}</p>
   <div class="flex items-center">
     <img :src="currentWeather.iconUrl" alt="weather icon" height="96" />
     <h4>{{ currentWeather.description }}</h4>
@@ -9,57 +9,65 @@
 
   <q-card class="my-card bg-primary text-white">
     <q-card-section>
-      <q-item>
+      <q-item class="info">
         <q-item-section top avatar>
           <q-avatar color="secondary" text-color="white" icon="thermostat" />
         </q-item-section>
 
-        <q-item-section side>
-          <strong text-color="white"
-            >min {{ currentWeather.tempMin }} - max
-            {{ currentWeather.tempMax }}</strong
-          >
+        <q-item-section side class="text-deep-purple-3 min-max">
+          <div>
+            <span class="text-deep-purple-3"> max</span>
+            <strong class="text-deep-purple-1">{{
+              currentWeather.tempMax
+            }}</strong>
+          </div>
+          <div>
+            <span class="text-deep-purple-3">min</span>
+            <strong class="text-deep-purple-1">{{
+              currentWeather.tempMin
+            }}</strong>
+          </div>
         </q-item-section>
       </q-item>
 
-      <q-item>
+      <q-item class="info">
         <q-item-section top avatar>
           <q-avatar color="secondary" text-color="white" icon="south_east" />
         </q-item-section>
 
-        <q-item-section>
+        <q-item-section class="text-deep-purple-3">
           <q-item-label>Pressure</q-item-label>
         </q-item-section>
 
-        <q-item-section side>
+        <q-item-section side class="text-deep-purple-1">
           <q-item-label>{{ currentWeather.pressure }}</q-item-label>
         </q-item-section>
       </q-item>
 
-      <q-item>
+      <q-item class="info">
         <q-item-section top avatar>
           <q-avatar color="secondary" text-color="white" icon="water_drop" />
         </q-item-section>
 
-        <q-item-section>
+        <q-item-section class="text-deep-purple-3">
           <q-item-label>Humidity</q-item-label>
         </q-item-section>
 
-        <q-item-section side>
+        <q-item-section side class="text-deep-purple-1">
           <q-item-label>{{ currentWeather.humidity }}</q-item-label>
         </q-item-section>
       </q-item>
 
-      <q-item>
+      <q-item class="info">
         <q-item-section top avatar>
           <q-avatar color="secondary" text-color="white" icon="air" />
         </q-item-section>
 
-        <q-item-section>
+        <q-item-section class="text-deep-purple-3">
           <q-item-label>Wind speed</q-item-label>
         </q-item-section>
 
-        <q-item-section side>
+        <q-item-section side class="text-deep-purple-1">
           <q-item-label>{{ currentWeather.windSpeed }}</q-item-label>
         </q-item-section>
       </q-item>
@@ -98,5 +106,26 @@ h5 {
 h1 {
   margin: 1rem auto;
   color: white;
+}
+
+.feel {
+  font-size: 2rem;
+}
+
+.info,
+input + div {
+  font-size: 1.4rem;
+}
+
+.min-max {
+  display: flex;
+  flex-grow: 1;
+  padding-right: 0;
+}
+.min-max > div {
+  width: 100%;
+  display: flex;
+  flex-grow: 1;
+  justify-content: space-between;
 }
 </style>
